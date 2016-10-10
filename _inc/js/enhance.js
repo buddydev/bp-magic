26a9da06168b6bf5976e8dc80098158e12db09e6
Ravi Sharma <ravivats89@gmail.com>
2016-10-10 18:48:36 +0530

    upload button in group manage and change profile photo tab design fix

--- _inc/js/enhance.js
+++ _inc/js/enhance.js
@@ -7,7 +7,7 @@ jQuery( document ).ready( function () {
 	var jq = jQuery;
 
 	/**** Page Load Actions *******************************************************/
-	jq( "#container select, :radio,:checkbox,:file" ).uniform( {
+	jq( "#container select, :radio,:checkbox" ).uniform( {
 		selectClass: 'uni-selector',
 		radioClass: 'uni-radio',
 		checkboxClass: 'uni-checker',
